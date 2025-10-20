e32d70c4957f6fe76c78cf0b3f27490518c847b5
aj1120 <152417087+aj1120@users.noreply.github.com>
2025-10-20 09:39:43 +0530

    test1 commit

--- airflow/airflow_job.py
+++ airflow/airflow_job.py
@@ -1,6 +1,96 @@
-x = 5
+from datetime import timedelta
+import uuid
+from airflow.utils.dates import days_ago
+from airflow import DAG
+from airflow.providers.google.cloud.operators.dataproc import DataprocCreateBatchOperator
+from airflow.providers.google.cloud.transfers.gcs_to_gcs import GCSToGCSOperator
+from airflow.utils.trigger_rule import TriggerRule
 
-for i in range(5):
-    if i > 3:
-        print(i)
-    break
+# DAG default arguments
+default_args = {
+    "owner": "airflow",
+    "start_date": days_ago(1),
+    "depends_on_past": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5)
+}
+
+with DAG(
+    dag_id="credit_card_transactions_dataproc_dag2",
+    default_args=default_args,
+    schedule_interval="0 5 * * *",
+) as dag:
+
+    # GCS Config
+    gcs_bucket = "avd-bucket-credit-card-analysis"
+    file_pattern = "transactions/transactions_"
+    source_prefix = "transactions/"
+    archive_prefix = "archive/"
+
+    # Generate unique batch IDs
+    batch_id_users = f"load-users-batch-{str(uuid.uuid4())[:8]}"
+    batch_id_txns = f"credit-card-batch-{str(uuid.uuid4())[:8]}"
+
+    #Dataproc Job 1: Load users to BigQuery
+    load_users_task = DataprocCreateBatchOperator(
+        task_id="load_users_to_bq",
+        batch={
+            "pyspark_batch": {
+                "main_python_file_uri": f"gs://{gcs_bucket}/spark_job/load_users_to_bq.py"
+            },
+            "runtime_config": {
+                "version": "2.2.61",
+            },
+            "environment_config": {
+                "execution_config": {
+                    "service_account": "813125969429-compute@developer.gserviceaccount.com",
+                    "network_uri": "projects/feisty-flow-466306-f6/global/networks/default",
+                    "subnetwork_uri": "projects/feisty-flow-466306-f6/regions/us-central1/subnetworks/default",
+                }
+            },
+        },
+        batch_id=batch_id_users,
+        project_id="feisty-flow-466306-f6",
+        region="us-central1",
+        gcp_conn_id="google_cloud_default",
+    )
+
+    # Dataproc Job 2: Process transactions, "version": "2.2",
+    process_txns_task = DataprocCreateBatchOperator(
+        task_id="run_credit_card_processing_job",
+        batch={
+            "pyspark_batch": {
+                "main_python_file_uri": f"gs://{gcs_bucket}/spark_job/spark_job.py"
+            },
+            "runtime_config": {
+                "version": "2.2.61",
+            },
+            "environment_config": {
+                "execution_config": {
+                    "service_account": "813125969429-compute@developer.gserviceaccount.com",
+                    "network_uri": "projects/feisty-flow-466306-f6/global/networks/default",
+                    "subnetwork_uri": "projects/feisty-flow-466306-f6/regions/us-central1/subnetworks/default",
+                }
+            },
+        },
+        batch_id=batch_id_txns,
+        project_id="feisty-flow-466306-f6",
+        region="us-central1",
+        gcp_conn_id="google_cloud_default",
+    )
+
+    # Archive processed files
+    move_files_to_archive = GCSToGCSOperator(
+        task_id="move_files_to_archive",
+        source_bucket=gcs_bucket,
+        source_object=source_prefix,
+        destination_bucket=gcs_bucket,
+        destination_object=archive_prefix,
+        move_object=True,
+        trigger_rule=TriggerRule.ALL_SUCCESS,
+    )
+
+    # DAG Flow
+    #load_users_task >> process_txns_task >> move_files_to_archive
+    process_txns_task >> move_files_to_archive
+
